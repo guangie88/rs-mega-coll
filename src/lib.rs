1759f72d5cf85d02c03663e0e89889a4804a59f9
Chen Weiguang <chen.weiguang@gmail.com>
2018-03-09 14:54:27 +0800

    Add in fs util functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
+#![cfg_attr(feature = "cargo-clippy", deny(warnings))]
+
 extern crate chrono;
 extern crate failure;
 #[macro_use]
 extern crate failure_derive;
+extern crate fs2;
 extern crate regex;
 extern crate serde;
 #[macro_use]
--- src/util/fs.rs
+++ src/util/fs.rs
@@ -1,1 +1,38 @@
+use error::{ErrorKind, Result};
+use error::custom::PathError;
+use failure::ResultExt;
+use fs2::FileExt;
+use std::fs::{File, OpenOptions};
+use std::io::Read;
+use std::path::Path;
 
+pub fn read_from_file<P: AsRef<Path>>(p: P) -> Result<String> {
+    let mut buf = String::new();
+    let p = p.as_ref();
+
+    let mut file = File::open(p)
+        .map_err(|e| PathError::new(p.to_string_lossy().to_string(), e))
+        .context(ErrorKind::FileIo)?;
+
+    file.read_to_string(&mut buf)
+        .context(ErrorKind::FileIo)?;
+    Ok(buf)
+}
+
+pub fn lock_file<P: AsRef<Path>>(file_path: P) -> Result<File> {
+    let file_path = file_path.as_ref();
+
+    let flock = OpenOptions::new()
+        .write(true)
+        .create(true)
+        .open(file_path)
+        .map_err(|e| PathError::new(file_path, e))
+        .context(ErrorKind::LockFileOpen)?;
+
+    flock
+        .try_lock_exclusive()
+        .map_err(|e| PathError::new(file_path, e))
+        .context(ErrorKind::LockFileExclusiveLock)?;
+
+    Ok(flock)
+}
