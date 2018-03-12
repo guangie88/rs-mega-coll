c70dd488f28a61a87ebc42adc501b873d47d2453
Chen Weiguang <chen.weiguang@gmail.com>
2018-03-12 13:54:14 +0800

    Add serde impl for entry expiry

--- src/conf/cache.rs
+++ src/conf/cache.rs
@@ -1,7 +1,9 @@
+use serde_humantime;
 use std::time::Duration;
 
 #[derive(Deserialize, Debug)]
 pub struct Config {
     pub connection_url: String,
+    #[serde(with = "serde_humantime", default)]
     pub entry_expiry_duration: Duration,
 }
